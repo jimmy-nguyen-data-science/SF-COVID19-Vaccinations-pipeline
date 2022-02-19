d483bd999f6f0e5592368ff492085756e807a2ff
Jimmy Nguyen <ads507team5@gmail.com>
2022-02-19 19:20:07 +0000

    fix staging models and dim table

--- models/marts/core/dim_monthy_boosters.sql
+++ models/marts/core/dim_monthy_boosters.sql
@@ -4,28 +4,18 @@ WITH month_year AS (
     FROM
         {{ ref('stg_monthly') }}
 ),
-new_boost_recip AS (
-    SELECT
-        *
-    FROM
-        {{ ref('stg_boost_recip') }}
-),
 final as (
     SELECT
-        year,
-        month,
-        sum(new_boost_recip.new_booster_recipients) AS total_booster_recipients
+        yearmonth,
+        sum(new_booster_recipients) AS total_booster_recipients
     FROM
-        month_year
-        INNER JOIN new_boost_recip using(date_administered)
+        month_year    
     GROUP BY
-        year,
-        month
-    ORDER BY
-        year,
-        month
+        yearmonth
 )
 SELECT
     *
 FROM
     final
+ORDER BY
+        yearmonth
